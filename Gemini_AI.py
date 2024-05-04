b6bb28b59dc3ced32477f7df5166d51c621a7b23
YZ <kokyenzein@gmail.com>
2024-05-04 00:32:37 -0400

    gemini_ai init

---
+++ Gemini_AI.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+project_id = os.getenv('PROJECT_ID')
+location = os.getenv('LOCATION')
+google_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+# Set the GOOGLE_APPLICATION_CREDENTIALS environment variable
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_credentials
